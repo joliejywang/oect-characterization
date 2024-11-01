9df573a02c4b3979ea08c3010164191c04652473
Jolie Wang <jolie.j.wang@gmail.com>
2024-11-01 01:30:34 -0500

    complete output curve gui

--- output_curve_gui.py
+++ output_curve_gui.py
@@ -22,6 +22,168 @@ from measurement_functions import generateLinSweep, runListSweep
 # gui window
 window = tkinter.Tk()
 
+# get individual variables
+def get_vg_init():
+    Vg_init = float(vg_init_entry.get())
+    return Vg_init
+
+def get_vg_final():
+    Vg_final = float(vg_final_entry.get())
+    return Vg_final
+
+def get_vg_step():
+    Vg_step = float(vg_step_entry.get())
+    return Vg_step
+
+def get_vd_init():
+    Vd_init = float(vd_init_entry.get())
+    return Vd_init
+
+def get_vd_final():
+    Vd_final = float(vd_final_entry.get())
+    return Vd_final
+
+def get_time_per_point():
+    time_per_point = float(time_per_point_entry.get())
+    return time_per_point
+
+def get_points_per_sweep():
+    points_per_sweep = int(points_per_sweep_entry.get())
+    return points_per_sweep
+
+def get_reverse_sweep():
+    reverse_sweep = reverse_sweep_is_checked.get()
+    return reverse_sweep
+
+def get_ig_range():
+    Ig_range = float(ig_range_entry.get())
+    return Ig_range
+
+def get_id_range():
+    Id_range = float(id_range_entry.get())
+    return Id_range
+
+def get_t_hold_before_meas():
+    t_hold_before_meas = float(time_hold_entry.get())
+    return t_hold_before_meas
+
+# check if settings are correct
+def set_settings():
+    # get entered labels
+    Vg_init = get_vg_init()
+    Vg_final = get_vg_final()
+    Vg_step = get_vg_step()
+    Vd_init = get_vd_init()
+    Vd_final = get_vd_final()
+    time_per_point = get_time_per_point()
+    points_per_sweep = get_points_per_sweep()
+    reverse_sweep = get_reverse_sweep()
+    Ig_range = get_ig_range()
+    Id_range = get_id_range()
+    t_hold_before_meas = get_t_hold_before_meas()
+
+    messagebox.showinfo("Inputs", f"Inputs Entered:"
+                                  f"\nVg init: {Vg_init}"
+                                  f"\nVg final: {Vg_final}"
+                                  f"\nVg step: {Vg_step}"
+                                  f"\nVd init: {Vd_init}"
+                                  f"\nVd final: {Vd_final}"
+                                  f"\nTime per point: {time_per_point}"
+                                  f"\nPoints per sweep: {points_per_sweep}"
+                                  f"\nReverse sweep: {reverse_sweep}"
+                                  f"\nIg range: {Ig_range}"
+                                  f"\nId range: {Id_range}"
+                                  f"\nTime Hold: {t_hold_before_meas}")
+
+def run_output_curves():
+    # initialize global variables
+    global output, Vd_output, Vg_values_output
+
+    # get entered labels
+    Vg_init = get_vg_init()
+    Vg_final = get_vg_final()
+    Vg_step = get_vg_step()
+    Vd_init = get_vd_init()
+    Vd_final = get_vd_final()
+    time_per_point = get_time_per_point()
+    points_per_sweep = get_points_per_sweep()
+    reverse_sweep = get_reverse_sweep()
+    Ig_range = get_ig_range()
+    Id_range = get_id_range()
+    t_hold_before_meas = get_t_hold_before_meas()
+
+    # Generate array of gate voltage values based on user input
+    Vg_values_output = np.empty((round((Vg_final - Vg_init) / Vg_step) + 1,))
+    for i in range(0, len(Vg_values_output)):
+        if Vg_init > Vg_final:
+            step = -1 * abs(Vg_step)
+        else:
+            step = abs(Vg_step)
+        Vg_values_output[i] = round(Vg_init + step * i, 4)
+
+    # Generate sweep for drain voltage based on user input
+    vd_list = generateLinSweep(points_per_sweep, Vd_init, Vd_final, reverse=reverse_sweep)
+    vg_list = "{:.1E}".format(Vg_values_output[0])
+
+    # Create measurement parameters
+    # parameters format (list of strings, units of seconds, amps, volts):
+    # [(0) measurment points,(1) time per point,(2) measurement delay,(3) aquisition time,
+    #  (4) gate current range,(5) drain current range,(6) gate points,(7) drain points,
+    #  (8) pre-measurement voltage hold time]
+    parameters = []
+    parameters.append("{:.0f}".format(len(vd_list.split(','))))  # 0
+    parameters.append("{:.1E}".format(time_per_point))  # 1
+    parameters.append("{:.1E}".format(time_per_point / 2))  # 2
+    parameters.append("{:.1E}".format((time_per_point / 2) * 0.8))  # 3
+    parameters.append("{:.0E}".format(Ig_range))  # 4
+    parameters.append("{:.0E}".format(Id_range))  # 5
+    parameters.append("1")  # 6
+    parameters.append("{:.0f}".format(len(vd_list.split(','))))  # 7
+    parameters.append(t_hold_before_meas)
+
+    # Create empty array to store output data
+    output = np.empty((len(Vg_values_output), int(parameters[0]), 4))
+
+    # Run output curve for each gate voltage and plot results
+    output[0] = runListSweep(parameters, vg_list, vd_list)
+    Vd_output = np.asarray([float(i) for i in vd_list.split(',')])
+    plt.plot(Vd_output, output[0, :, 2], label="$V_{GS}$ = " + str(Vg_values_output[0]))
+
+    for i in range(1, len(output)):
+        vg_list = "{:.1E}".format(Vg_values_output[i])
+        output[i] = runListSweep(parameters, vg_list, vd_list)
+        plt.plot(Vd_output, output[i, :, 2], label="$V_{GS}$ = " + str(Vg_values_output[i]))
+
+    plt.gca().invert_yaxis()
+    plt.xlabel("$V_{DS}$ (V)")
+    plt.ylabel("$I_{DS}$ (A)")
+    plt.title("Output curves")
+    plt.legend()
+
+    # Turn instrument output off after measurement
+    inst.write(":outp1 off")
+    inst.write(":outp2 off")
+
+def save_output_curve_measurement():
+    # initialize global variables
+    global output, Vd_output, Vg_values_output
+
+    # get filename input
+    filename = filename_entry.get()
+
+    # Create the path before trying to save
+    # path = "C:\\Users\\skeen\\Dropbox\\000_Postdoctoral_Research\\Voltage-dependent mobility with Sophia\\OECT Data\\2023-02-20_pg1T2-g5T2_100mM_NaCl\\"
+    folder = polymer + "\\" + device + "\\"
+
+    outp_data = pd.DataFrame()
+    outp_data["Time (s)"] = output[0, :, 1]
+    outp_data["Vds (V)"] = Vd_output
+    for i in range(0, len(output)):
+        outp_data["Ids (A) at Vg = " + str(Vg_values_output[i]) + " V"] = output[i, :, 2]
+        outp_data["Igs (A) at Vg = " + str(Vg_values_output[i]) + " V"] = output[i, :, 0]
+
+    outp_data.to_csv(path + folder + filename)
+
 # create widgets
 # create input fields for setting
 Label(window, text="Initial Vg: ").grid(row=0, column=0)
@@ -33,8 +195,8 @@ vg_final_entry = Entry(window)
 vg_final_entry.grid(row=1, column=1)
 
 Label(window, text="Vg Step: ").grid(row=2, column=0)
-vd_step_entry = Entry(window)
-vd_step_entry.grid(row=2, column=1)
+vg_step_entry = Entry(window)
+vg_step_entry.grid(row=2, column=1)
 
 Label(window, text="Initial Vd: ").grid(row=3, column=0)
 vd_init_entry = Entry(window)
